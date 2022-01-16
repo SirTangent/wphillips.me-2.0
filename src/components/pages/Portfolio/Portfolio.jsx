77b384af5ef2750e7fa6ac7d2144db285766cf93
Wyatt Phillips <phillipsw1@wit.edu>
2022-01-16 17:16:02 -0500

    Remove image links

--- src/components/pages/Portfolio/Portfolio.jsx
+++ src/components/pages/Portfolio/Portfolio.jsx
@@ -1,14 +1,9 @@
 import React from "react";
-import image1 from "../../../images/portfolio/project-01.jpg";
-import image2 from "../../../images/portfolio/project-02.jpg";
-import image3 from "../../../images/portfolio/project-03.jpg";
-import image4 from "../../../images/portfolio/project-04.jpg";
 import PortfolioItem from "./PortfolioItem";
 
 const portfolioData = [
   {
     id: 1,
-    image: image1,
     title: "Space Bootstrap 5 Multipurpose Template",
     link: "#0",
     description:
@@ -16,7 +11,6 @@ const portfolioData = [
   },
   {
     id: 2,
-    image: image2,
     title: "eCommerce HTML Bootstrap 5 UI Kit",
     link: "#0",
     description:
@@ -24,7 +18,6 @@ const portfolioData = [
   },
   {
     id: 3,
-    image: image3,
     title: "Basic – Tailwind CSS Template",
     link: "#0",
     description:
@@ -32,7 +25,6 @@ const portfolioData = [
   },
   {
     id: 4,
-    image: image4,
     title: "Gearix Car repair App",
     link: "#0",
     description:
